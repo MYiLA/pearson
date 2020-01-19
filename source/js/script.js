506e6379cab28ab3cf4d6d983ff955eb85c1fcaf
MYiLA <polus180@ya.ru>
2020-01-19 11:17:36 +0300

    header adaptive and stiky created

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// липкая шапка
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 1) {
+    $('.header__main-nav').addClass('fixed');
+    $('.header__main-nav').removeClass('static');
+  
+  } else {
+    $('.header__main-nav').addClass('static');
+    $('.header__main-nav').removeClass('fixed');
+  }
+});
