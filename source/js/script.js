f43a61ad9f535ffa6dadab0605260e3eb775216c
MYiLA <polus180@ya.ru>
2020-01-30 07:02:01 +0300

    fix js

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,80 @@
 
 const tabletWidth = 900;
 
+// URL запросы для смены радиальных меню лендингов
+
+const getUrlParams = search => {
+  const params = {};
+  search.substr(1).split("&").forEach(part => {
+    const [key, value] = part.split("=");
+    params[key] = decodeURIComponent(value);
+  });
+  return params;
+};
+
+let requestUrl = getUrlParams(window.location.search).wheel || 'superieur';
+
+// переключение вкладок радиального меню
+
+const superieurBookmarkElement = document.querySelector('.main-nav__bookmark-item--superieur');
+const superieurWheelElement = document.querySelector('.wheel-promo--superieur');
+
+const etrangereBookmarkElement = document.querySelector('.main-nav__bookmark-item--etrangere');
+const etrangereWheelElement = document.querySelector('.wheel-promo--etrangere');
+
+let toggleElements = function (openElement, closeElement) {
+  openElement.classList.add('active');
+  closeElement.classList.remove('active');
+}
+
+let activateSuperieur = function () {
+  superieurBookmarkElement.classList.add('active');
+  etrangereBookmarkElement.classList.remove('active');
+
+  let closeMenu = function () {
+    superieurWheelElement.classList.add('active');
+    etrangereWheelElement.classList.remove('active');
+  }
+  superieurWheelElement.classList.remove('dissolve-hidden');
+  etrangereWheelElement.classList.add('dissolve-hidden');
+
+  etrangereWheelElement.classList.remove('dissolve-show');
+  superieurWheelElement.classList.add('dissolve-show');
+
+  setTimeout(closeMenu, 600);
+}
+
+let activateEtrangere = function () {
+  etrangereBookmarkElement.classList.add('active');
+  superieurBookmarkElement.classList.remove('active');
+
+  const closeMenu = function () {
+    etrangereWheelElement.classList.add('active');
+    superieurWheelElement.classList.remove('active');
+  }
+
+  etrangereWheelElement.classList.remove('dissolve-hidden');
+  superieurWheelElement.classList.add('dissolve-hidden');
+
+  superieurWheelElement.classList.remove('dissolve-show');
+  etrangereWheelElement.classList.add('dissolve-show');
+  setTimeout(closeMenu, 600);
+}
+
+const openBookmark = () => {
+  if (requestUrl === "superieur") {
+    activateSuperieur()
+  }
+
+  if (requestUrl === "etrangere") {
+    activateEtrangere()
+  } else {
+    $('.main-nav__bookmark-item--superieur main-nav__bookmark').trigger('click');
+  }
+}
+
+openBookmark();
+
 // колесо ///
 
 const menus = [...document.querySelectorAll('.wheel-promo')];
@@ -142,13 +216,12 @@ formVideoCloseElement.addEventListener('click', onClickFormVideo);
 const formLandingElement = document.querySelector('.landing-promo__btn--video');
 const provokeElement = document.querySelector('.provoke__link--form');
 const promoBtnElement = document.querySelector('.landing-promo__btn--form-2');
-const landingPlayElement = document.querySelector('.landing-media__play-btn');
 
 const videoProvokeElement = document.querySelector('.provoke__link--video');
 
 formLandingElement.addEventListener('click', onClickFormVideo);
 videoProvokeElement.addEventListener('click', onClickFormVideo);
-landingPlayElement.addEventListener('click', onClickFormVideo);
+
 
 provokeElement.addEventListener('click', onClickFormContact);
 promoBtnElement.addEventListener('click', onClickFormContact);
@@ -172,78 +245,6 @@ videoSubmitElement.addEventListener('click', function (evt) {
   onClickFormVideo();
   onClickNavVideo();
 });
-
-// URL запросы для смены радиальных меню лендингов
-
-const getUrlParams = search => {
-  const params = {};
-  search.substr(1).split("&").forEach(part => {
-    const [key, value] = part.split("=");
-    params[key] = decodeURIComponent(value);
-  });
-  return params;
-};
-
-let requestUrl = getUrlParams(window.location.search).wheel || 'superieur';
-
-// переключение вкладок радиального меню
-
-const superieurBookmarkElement = document.querySelector('.main-nav__bookmark-item--superieur');
-const superieurWheelElement = document.querySelector('.wheel-promo--superieur');
-
-const etrangereBookmarkElement = document.querySelector('.main-nav__bookmark-item--etrangere');
-const etrangereWheelElement = document.querySelector('.wheel-promo--etrangere');
-
-let toggleElements = function (openElement, closeElement) {
-  openElement.classList.add('active');
-  closeElement.classList.remove('active');
-}
-
-let activateSuperieur = function () {
-  superieurBookmarkElement.classList.add('active');
-  etrangereBookmarkElement.classList.remove('active');
-
-  let closeMenu = function () {
-    superieurWheelElement.classList.add('active');
-    etrangereWheelElement.classList.remove('active');
-  }
-  superieurWheelElement.classList.remove('dissolve-hidden');
-  etrangereWheelElement.classList.add('dissolve-hidden');
-
-  etrangereWheelElement.classList.remove('dissolve-show');
-  superieurWheelElement.classList.add('dissolve-show');
-
-  setTimeout(closeMenu, 600);
-}
-
-let activateEtrangere = function () {
-  etrangereBookmarkElement.classList.add('active');
-  superieurBookmarkElement.classList.remove('active');
-
-  const closeMenu = function () {
-    etrangereWheelElement.classList.add('active');
-    superieurWheelElement.classList.remove('active');
-  }
-
-  etrangereWheelElement.classList.remove('dissolve-hidden');
-  superieurWheelElement.classList.add('dissolve-hidden');
-
-  superieurWheelElement.classList.remove('dissolve-show');
-  etrangereWheelElement.classList.add('dissolve-show');
-  setTimeout(closeMenu, 600);
-}
-
-const openBookmark = () => {
-  if (requestUrl === "superieur") {
-    activateSuperieur()
-  }
-
-  if (requestUrl === "etrangere") {
-    activateEtrangere()
-  } 
-}
-
-openBookmark();
 
 // динамическая смена подписей к слайдеру
 
